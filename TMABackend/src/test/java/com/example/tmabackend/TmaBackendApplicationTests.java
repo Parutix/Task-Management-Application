4dd6ab60e9cdd1e377a5ddbd5fc2703b8c501b98
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-02-08 19:21:27 +0200

    created foundation for the project

---
+++ TMABackend/src/test/java/com/example/tmabackend/TmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
