bcd16450e225178ff0c470925042fb06ce631aa3
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-02-09 13:53:53 +0200

    finalised register
    users can now register and be introduced into the database

--- TMABackend/src/main/java/org/example/tmabackend/TmaBackendApplication.java
+++ TMABackend/src/main/java/org/example/tmabackend/TmaBackendApplication.java
@@ -2,12 +2,18 @@ package org.example.tmabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class TmaBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TmaBackendApplication.class, args);
     }
+
 
 }
