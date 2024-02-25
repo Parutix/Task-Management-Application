c0dd2a0109758f3b0e2b50f86efde046e90ddad8
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-02-25 21:45:01 +0200

    [WIP] home page

--- TMAFrontend/TMAFrontend/src/pages/Home/HomePage.jsx
+++ TMAFrontend/TMAFrontend/src/pages/Home/HomePage.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./HomePage.css";
 
 const HomePage = () => {
-  return <div className="home_background_container"></div>;
+  return (
+    <div className="home_background_container">
+      <h1>Task Lists</h1>
+      <button className="button">Create New List</button>
+    </div>
+  );
 };
 
 export default HomePage;
